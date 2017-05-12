15df5753a5d5eee979476e799f2c60b7bbaef4c1
Nicolas Oga <2112.oga@gmail.com>
2017-05-12 15:15:56 -0300

    TYPO

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  API_URL:     $PROCESS_ENV_API_URL || 'http://localhost:3000'
+  API_URL:     $PROCESS_ENV_API_URL || 'http://localhost:3000',
   FACEBOOK_ID: $PROCESS_ENV_FACEBOOK_ID || '1488551384779862'
 };
