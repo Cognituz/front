e176d0f15dacc4a8917f04d71ab9194fb1373a71
Nicolas Oga <2112.oga@gmail.com>
2017-05-13 19:37:23 -0300

    TYPO

--- app/config/satellizer.js
+++ app/config/satellizer.js
@@ -3,6 +3,6 @@ const {API_URL, FACEBOOK_ID} = require('config');
 module.exports = $authProvider => {
   'ngInject';
 
-  $authProvider.baseUrl = `#${API_URL}/v1`;
+  $authProvider.baseUrl = `${API_URL}/v1`;
   $authProvider.facebook({clientId: FACEBOOK_ID});
 };
