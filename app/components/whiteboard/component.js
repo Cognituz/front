285505dc15849c5ae4bfbd1853e002e1d5c101d5
Nicolas Oga <2112.oga@gmail.com>
2017-06-25 20:34:02 -0300

    Whiteboard feature: text interstion

--- app/components/whiteboard/component.js
+++ app/components/whiteboard/component.js
@@ -10,7 +10,7 @@ module.exports = {
   },
 
   controller: class {
-    currentTool        = 'pencil';
+    currentTool = 'pencil';
     currentToolOptions = {
       thickness: 5,
       color:    "#000000"
@@ -31,19 +31,28 @@ module.exports = {
         this.ctx.strokeStyle              = "rgba(0,0,0,1)";
         this.ctx.lineJoin                 = "square";
         this.ctx.lineCap                  = "square";
+      },
+
+      textInserter(toolOptions = {}) {
+        this.ctx.globalCompositeOperation = "source-over";
+        this.textAlign                    = 'center';
+        this.ctx.textBaseline             = 'middle';
+        this.ctx.font                     = '3vw Dosis';
+        this.ctx.fillStyle                = 'black';
       }
     };
 
     // To store signals
     signals = [];
 
-    constructor($attrs, $element, $scope, $timeout, $window) {
+    constructor($attrs, $element, $mdDialog, $scope, $timeout, $window) {
       'ngInject';
-      this.$attrs   = $attrs;
-      this.$element = $element;
-      this.$scope   = $scope;
-      this.$timeout = $timeout;
-      this.$window  = $window;
+      this.$attrs    = $attrs;
+      this.$element  = $element;
+      this.$mdDialog = $mdDialog;
+      this.$scope    = $scope;
+      this.$timeout  = $timeout;
+      this.$window   = $window;
     }
 
     get $canvas() {
@@ -74,12 +83,12 @@ module.exports = {
         this.$element.on("mousedown touchstart", ev => {
           ev.preventDefault();
           this.mouseDown = true;
-          this.draw(ev)
+          this.drawLine(ev)
         });
 
         this.$element.on("mousemove touchmove", ev => {
           ev.preventDefault();
-          this.draw(ev);
+          this.drawLine(ev);
         });
 
         this.$element.on("mouseup touchend", ev => {
@@ -91,12 +100,18 @@ module.exports = {
           ev.preventDefault();
           this.mouseDown = false;
         });
+
+        this.$element.on("click touchstart", ev => {
+          ev.preventDefault();
+          this.drawText(ev);
+        })
       }
     }
 
     // Although canvas is set to fill container via layout-fill
     // we still need to resize it manually to update it's inner state
-    // and really fill it's contaier
+    // and really fill it's contaier. Otherwise the internal width and height
+    // remains the same and causes drawLine errors.
     resize(delay) {
       this.resizing = true
       this.doResize(delay).then(_ => this.resizing = false);
@@ -110,35 +125,14 @@ module.exports = {
       }, delay);
     }
 
-    draw(ev) {
-      const [pointA, pointB] = this.getLine(ev);
-      if (!this.mouseDown || this.resizing) { return; }
-      this.drawSegment(pointA, pointB);
-    }
-
     getLine(ev) {
-      const offset = this.$element.offset();
-      const pointB = [
-        ev.pageX - offset.left,
-        ev.pageY - offset.top
-      ];
+      const pointB = this.extractCoordinates(ev);
       const pointA = this.lastPoint;
       const line = [pointA, pointB];
 
       this.lastPoint = pointB;
 
       return line;
-    }
-
-    getRelativeCoords([x1,y1], [x2,y2], [width, height]) {
-      const _coords = [];
-
-      const _x1 = (x1 * this.$canvas.width()) / parseInt(width);
-      const _y1 = (y1 * this.$canvas.height()) / parseInt(height);
-      const _x2 = (x2 * this.$canvas.width()) / parseInt(width);
-      const _y2 = (y2 * this.$canvas.height()) / parseInt(height);
-
-      return [[_x1,_y1], [_x2,_y2]];
     }
 
     // Remote capabilities
@@ -163,11 +157,45 @@ module.exports = {
     storeSignal(signal) { this.signals.push(signal); }
 
     // Transmitable signals
-    drawSegment(pointA, pointB) {
+    drawLine(ev) {
+      const [pointA, pointB] = this.getLine(ev);
+      if (
+        ['pencil', 'eraser'].indexOf(this.currentTool) === -1 ||
+        !this.mouseDown ||
+        this.resizing
+      ) { return; }
+      this.doDrawLine(pointA, pointB);
+    }
+
+    doDrawLine(pointA, pointB) {
       const commonArgs = [pointA, pointB, this.currentTool, this.currentToolOptions];
       this.$drawSegment(...commonArgs);
       const canvasSize = [this.$canvas.width(), this.$canvas.height()];
       this.signal("$drawSegment", ...commonArgs, canvasSize);
+    }
+
+    drawText(ev) {
+      if (this.currentTool !== 'textInserter') return;
+      this.applyTool()
+      const point = this.extractCoordinates(ev);
+      this.getTextToInsert(ev)
+        .then(text => this.doDrawText(point, text));
+    }
+
+    doDrawText(point, text) {
+      this.$drawText(point, text);
+      this.signal('$drawText', point, text);
+    }
+
+    getTextToInsert(ev) {
+      return this.$mdDialog.show(
+        this.$mdDialog
+          .prompt()
+          .placeholder('Texto a insertar')
+          .targetEvent(ev)
+          .cancel('Cancelar')
+          .ok('Insertar')
+      );
     }
 
     clear() {
@@ -197,6 +225,11 @@ module.exports = {
       this.ctx.restore();
     }
 
+    $drawText([x,y], text) {
+      this.applyTool('textInserter');
+      this.ctx.fillText(text, x, y);
+    }
+
     $clear() { this.ctx.clearRect(0,0, this.$canvas.width(), this.$canvas.height()); }
 
     $setTool(toolName, toolOptions) {
@@ -209,8 +242,31 @@ module.exports = {
       this.ctx.lineWidth = (val * this.canvas.width)/800;
     }
 
-    applyTool(tool, toolOptions, forceUpdate) {
+    applyTool(
+      tool = this.currentTool,
+      toolOptions = this.currentToolOptions
+    ) {
       this.tools[tool].call(this, toolOptions);
+    }
+
+    getRelativeCoords([x1,y1], [x2,y2], [width, height]) {
+      const _coords = [];
+
+      const _x1 = (x1 * this.$canvas.width()) / parseInt(width);
+      const _y1 = (y1 * this.$canvas.height()) / parseInt(height);
+      const _x2 = (x2 * this.$canvas.width()) / parseInt(width);
+      const _y2 = (y2 * this.$canvas.height()) / parseInt(height);
+
+      return [[_x1,_y1], [_x2,_y2]];
+    }
+
+    extractCoordinates(ev) {
+      const offset = this.$element.offset();
+
+      return [
+        ev.pageX - offset.left,
+        ev.pageY - offset.top
+      ];
     }
   }
 };
